d2ea587e9d64ab88c8c1fc59d1076e243a9b5b47
maksim-smirnov <maksim.smirnov@gmail.com>
2021-06-17 11:03:30 +0300

    Сщььше June 17 with list of objects (holes)

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # todo 2. set up a set of objects (field, plants, farmer, facilities, tools, enemies, counter)
 # todo 3. create interaction methods
 # todo 4. create interface elements
-# To check June 17
 
 # Game Starts here
 import turtle
@@ -10,7 +9,15 @@ screen = turtle.Screen()
 screen.screensize(800, 555)
 screen.bgpic('Background.png')
 
+def write_coordinates(X:int, Y:int):
+    print(X, Y)
+
+
+screen.onclick(write_coordinates)
+
+#mainloop - должен быть последним!
 screen.mainloop()
+
 
 #screen.listen()
 #turtle_tom = TurtleParent(pos_x=-60, pos_y=-60, t_color="grey")
