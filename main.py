ebe6237b997600d627b36f49a2b9987aac573b69
maksim-smirnov <maksim.smirnov@gmail.com>
2021-06-17 10:35:49 +0300

    June 17 test

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 # todo 1. draw the map of the farm
 # todo 2. set up a set of objects (field, plants, farmer, facilities, tools, enemies, counter)
 # todo 3. create interaction methods
+# todo 4. create interface elements
+# To check June 17
+
+# Game Starts here
+import turtle
+screen = turtle.Screen()
+screen.screensize(800, 555)
+screen.bgpic('Background.png')
+
+screen.mainloop()
+
+#screen.listen()
+#turtle_tom = TurtleParent(pos_x=-60, pos_y=-60, t_color="grey")
+#turtle_tom.chaser = True
+#turtle_jerry = TurtleParent(pos_x=60, pos_y=60, t_color="purple")
+#turtle_tuffy = TurtleParent(pos_x=-100, pos_y=230, t_color="black")
+#turtle_tuffy.hideturtle()
