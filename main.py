575df921d36a144748e01f5d576adc8a6006a46b
SergeySelin <sdfs.kdfb.kb.bYTDH4udcwjg$%#>
2021-10-29 13:16:56 +0300

    29 Oct 2021 update

--- main.py
+++ main.py
@@ -3,29 +3,164 @@
 # todo 3. create interaction methods
 # todo 4. create interface elements
 # todo 5. create gifs for progress bar and locate near the hole
+# todo 7.Добавить к игре функции полива на левую кнопку мыши и посадки/сбора урожая на правую.
+# todo 8.На раунд даётся фиксированное количество времени (минута, например). Цель -- набрать максимальное число очков.
+# todo 9.Очки начисляются за собранные морковки, чем она спелее, тем лучше.
+#        После максимальной спелости морковка начинает сохнуть, и число очков за неё уменьшается.
+
+# todo actions: 1. Сажать морковку - левая клавиша мышки
+# todo actions: 2. Собирать морковку - левая клавиша мышки
+# todo actions: 3. Поливать лунку - правая клавиша мышки
+
 
 # Game Starts here
 import turtle
+from objects import *
+from constants import *
+import random
 
 
-#screen = turtle.Screen()
-#screen.screensize(800, 555)
-#screen.bgpic('Background.png')
-#screen.colormode(255)
-
-
-import objects
-
-
+screen = turtle.Screen()
+screen.screensize(800, 555)
+screen.bgpic('Background.png')
+screen.colormode(255)
 
 def write_coordinates(X:int, Y:int):
     print((X, Y))
 
-#screen.onclick(write_coordinates)
+# screen.onclick(write_coordinates)
+
+hole_list_upper_left_coordinates = [(-281.0, 92.0), (-254.0, 200.0), (-191.0, 211.0), (-219.0, 169.0), (-259.0, 133.0), (-222.0, 113.0),
+                                    (-255.0, 56.0)]
+
+hole_list_bottom_left_coordinates = [
+(-332.0, -99.0),
+(-317.0, -8.0),
+(-299.0, -54.0),
+(-259.0, -26.0),
+(-267.0, -92.0),
+ (-228.0, -59.0),
+ (-219.0, -118.0),
+ (-168.0, -69.0),
+ (-164.0, -135.0),
+ (-115.0, -93.0),
+ (-97.0, -161.0),
+ (-70.0, -125.0),
+ (-21.0, -117.0)]
+
+hole_list_right_coordinates = [(-5.0, 54.0), (67.0, 89.0), (116.0, 90.0)]
+
+
+screen.tracer(0)
+screen.update()
+
+
+#создаём лунки
+hole_list_all = []
+
+for hole in hole_list_upper_left_coordinates: hole_list_all.append(Hole(hole[0], hole[1]))
+for hole in hole_list_bottom_left_coordinates: hole_list_all.append(Hole(hole[0], hole[1]))
+for hole in hole_list_right_coordinates: hole_list_all.append(Hole(hole[0], hole[1]))
+
+# for i in range(10): hole_list_all[4].water(); time.sleep(1); screen.update()
+
+# carrot1 = Carrot()
+# carrot2 = Carrot()
+# hole_list_all[4].plant_plant(carrot1)
+# hole_list_all[2].plant_plant(carrot2)
+
+screen.update()
+
+def plant_carrot(X:int, Y:int, self=None):
+    """ Функция посадки моркови """
+# Функция принимает координаты щелчка по экрану и проверяет их с областью лунки, устанавливает лунку для посадки и сажает в неё мокровку
+    mouse_clicked = (X, Y)
+    min_distance_to_hole:int = 1000000
+    hole_selected: Hole
+    for hole in hole_list_all:
+        current_distance = hole.distance(mouse_clicked)
+        if current_distance < min_distance_to_hole: min_distance_to_hole = current_distance; hole_selected = hole
+
+    if hole_selected.distance(mouse_clicked) <= HOLE_HIT_RADIUS:
+        if not hole_selected.plant:
+            hole_selected.plant_plant(Carrot())
+            print(hole_selected.position())
+        else:
+            global reap_plant_counter # обращаемся к счетчику как глобальной переменной
+            reap_plant_counter += hole_selected.plant.reap_plant() # увеличиваем счетчик очков на величину спелости собранного овоща
+            print(reap_plant_counter, "- current score") # выводим в консоль количество собранного урожая в очках
+            hole_selected.clean_hole()
+            # print(f"Hole {hole_selected} is busy")
+
+        # print(hole_selected.plant)
+
+def plant_water(X:int, Y:int):
+    """ Функция полива лунки """
+# Функция принимает координаты щелчка по экрану и проверяет их с областью лунки, устанавливает лунку для полива и увеличивает уровень
+    mouse_clicked = (X, Y)
+    min_distance_to_hole:int = 1000000
+    hole_selected: Hole
+    for hole in hole_list_all:
+        current_distance = hole.distance(mouse_clicked)
+        if current_distance < min_distance_to_hole: min_distance_to_hole = current_distance; hole_selected = hole
+
+    if hole_selected.distance(mouse_clicked) <= HOLE_HIT_RADIUS:
+        hole_selected.water()
+        # print(hole_selected.plant)
+
+
+screen.onclick(plant_carrot, btn=1)
+screen.onclick(plant_water, btn=3)
+
+game_continues = True
+reap_plant_counter = 0;
+
+while game_continues:
+    time.sleep(TIME_REFRESH)
+    for hole in hole_list_all:
+        if hole.plant:
+            if random.randint(0, 20) == 5:
+                hole.plant.grow_plant()
+
+
+    #for i in range(1): hole_list_upper_left[4].water(); time.sleep(0.5)
+
+#     for i in range(15):
+#         carrot1.grow_plant()
+# #        indicator1.water_indicator_status(hole_list_upper_left[4].watered_degree)
+#         screen.update()
+#         print(f"Plant has grown for {i}")
+#         time.sleep(2)
+
+    # carrot1.reap_plant()
+    #hole_list_upper_left[4].clean_hole()
+
+    # for i in range(0,15):
+    #     for hole in hole_list_upper_left:
+    #         hole.water()
+    #         time.sleep(0.05)
+    #
+    # for i in range(0,15):
+    #     for hole in hole_list_upper_left:
+    #         hole.fertilize()
+    #         time.sleep(0.05)
+    #
+    # for i in range(0,15):
+    #     for hole in hole_list_upper_left:
+    #         hole.defertilize()
+    #         time.sleep(0.05)
+
+    screen.update()
+
+
+
+
+
+
 
 
 #mainloop - должен быть последним!
-#screen.mainloop()
+screen.mainloop()
 
 #screen.listen()
 #turtle_tom = TurtleParent(pos_x=-60, pos_y=-60, t_color="grey")
--- objects.py
+++ objects.py
@@ -3,30 +3,30 @@ import turtle
 import time
 from constants import *
 
-screen = turtle.Screen()
-screen.screensize(800, 555)
-screen.bgpic('Background.png')
-screen.colormode(255)
-
-hole_list_upper_left_coordinates = [(-281.0, 92.0), (-254.0, 200.0), (-191.0, 211.0), (-219.0, 169.0), (-259.0, 133.0), (-222.0, 113.0),
-                                    (-255.0, 56.0)]
-
-hole_list_bottom_left = [(-33.0, 305.0),
-(-332.0, -99.0),
-(-317.0, -8.0),
-(-299.0, -54.0),
-(-259.0, -26.0),
-(-267.0, -92.0),
- (-228.0, -59.0),
- (-219.0, -118.0),
- (-168.0, -69.0),
- (-164.0, -135.0),
- (-115.0, -93.0),
- (-97.0, -161.0),
- (-70.0, -125.0),
- (-21.0, -117.0)]
-
-hole_list_right = [(-5.0, 54.0), (67.0, 89.0), (116.0, 90.0)]
+# screen = turtle.Screen()
+# screen.screensize(800, 555)
+# screen.bgpic('Background.png')
+# screen.colormode(255)
+#
+# hole_list_upper_left_coordinates = [(-281.0, 92.0), (-254.0, 200.0), (-191.0, 211.0), (-219.0, 169.0), (-259.0, 133.0), (-222.0, 113.0),
+#                                     (-255.0, 56.0)]
+#
+# hole_list_bottom_left = [(-33.0, 305.0),
+# (-332.0, -99.0),
+# (-317.0, -8.0),
+# (-299.0, -54.0),
+# (-259.0, -26.0),
+# (-267.0, -92.0),
+#  (-228.0, -59.0),
+#  (-219.0, -118.0),
+#  (-168.0, -69.0),
+#  (-164.0, -135.0),
+#  (-115.0, -93.0),
+#  (-97.0, -161.0),
+#  (-70.0, -125.0),
+#  (-21.0, -117.0)]
+#
+# hole_list_right = [(-5.0, 54.0), (67.0, 89.0), (116.0, 90.0)]
 
 # hole_list_right2 = [(-12.0, 129.0)
 # (35.0, 191.0)
@@ -53,22 +53,28 @@ class Hole(turtle.Turtle):
         #self.pencolor("brown")
         self.watered_degree = 0
         self.manure_degree = 0
-        self.plant: Plant
+        self.plant: Plant = None
+        #-Test water indicator
+        self.indicator = WaterIndicator()
+        self.indicator.setposition(x-15,y)
 
     #метод для поливки лунки (один вызов метода +10%)
     def water(self):
+        """Метод увеличивает политость лунки на 1"""
         #Здесь надо сделать проверку на текущее значение <10 - иначе ничего не поливаем
         if self.watered_degree < 10:
             self.watered_degree += 1
-            self.color(0, 0, self.watered_degree * 25)
-            self.pencolor("brown")
+            self.indicator.water_indicator_status(self.watered_degree)
+#            self.color(0, 0, self.watered_degree * 25)
+#            self.pencolor("brown")
 
     #метод для высушивания лунки (один вызов метода -10%)
     def dehydrate(self):
         #Здесь надо сделать проверку на текущее значение <10 - иначе ничего не поливаем
         if self.watered_degree > 0 :
             self.watered_degree -= 1
-            self.color(0, 0, self.watered_degree * 25)
+            self.indicator.water_indicator_status(self.watered_degree)
+#            self.color(0, 0, self.watered_degree * 25)
 
     #метод для удобрения лунки (один вызов метода +1)
     def fertilize(self):
@@ -93,17 +99,24 @@ class Hole(turtle.Turtle):
         self.plant.setposition(self.xcor()+15,self.ycor(),)
         self.plant.hole = self
 
+    #test
+    def hole_indicator(self,indicator_type):
+        """Метод для отображения индикатора"""
+        self.indicator = indicator_type
+        self.indicator.setposition(self.xcor() - 15, self.ycor(), )
+        self.indicator.hole = self
+
     def clean_hole(self):
         """Очистка лунки"""
-        #del self.plant
-        #self.plant = None
-        print("test")
+        del self.plant
+        self.plant = None
+        print(f"Hole {self} cleared!")
 
 class Plant(turtle.Turtle):
     def __init__(self):
         super(Plant, self).__init__()
         self.penup()
-        self.screen = screen
+        # self.screen = screen
         self.hole: Hole
         self.ripeness = 0       #Спелость - от 0 до 2
         self.edibility: bool    #Съедобность
@@ -129,8 +142,10 @@ class Plant(turtle.Turtle):
     def reap_plant(self):
         self.clear()
         self.hideturtle()
+        print(self.ripeness, "points gained from this plant")  # выводим в консоль очки за собранный овощ, равные спелости
+        return self.ripeness #возвращаем величину спелости (ripeness)
         del self
-        """Нужно увеличить счётчик растения на единицу, если степень созревания 2"""
+
         #pass
 
 class Carrot(Plant):
@@ -142,61 +157,87 @@ class Carrot(Plant):
     def __init__(self):
         super(Carrot, self).__init__()
         for item in self.shape_type: self.screen.register_shape(item)
-        #self.screen.register_shape("carrot_g0.gif")
-        #self.screen.register_shape("carrot_g1.gif")
-        #self.screen.register_shape("carrot_g2.gif")
         self.shape(self.shape_type[0])
         #self.water_consume = 20
         #self.manure_consume = 10
 
+class WaterIndicator(Plant):
+    shape_type_WaterIndicator = ("indicatorBlueTransp0.gif", "indicatorBlueTransp1.gif", "indicatorBlueTransp2.gif", "indicatorBlueTransp3.gif")
+    def __init__(self):
+        super(WaterIndicator, self).__init__()
+        for item in self.shape_type_WaterIndicator: self.screen.register_shape(item)
+        self.shape(self.shape_type_WaterIndicator[0])
 
-#создаём лунки
-hole_list_upper_left = []
-
-for hole in hole_list_upper_left_coordinates:
-    hole_list_upper_left.append(Hole(hole[0],hole[1]))
-
-hole_list_upper_left[4].watered_degree = 9
-hole_list_upper_left[4].water()
-
-carrot1 = Carrot()
-carrot2 = Carrot()
-hole_list_upper_left[4].plant_plant(carrot1)
-hole_list_upper_left[2].plant_plant(carrot2)
-
-game_continues = True
-screen.tracer(0)
-screen.update()
-while game_continues:
-    time.sleep(TIME_REFRESH)
+    def water_indicator_status(self, water_degree):
+        if water_degree == 0:
+            self.shape(self.shape_type_WaterIndicator[0])
+        if water_degree > 0:
+            self.shape(self.shape_type_WaterIndicator[1])
+        if water_degree > 3:
+            self.shape(self.shape_type_WaterIndicator[2])
+        if water_degree > 6:
+            self.shape(self.shape_type_WaterIndicator[3])
 
 
-    #for i in range(1): hole_list_upper_left[4].water(); time.sleep(0.5)
+# screen.tracer(0)
+# screen.update()
 
-    for i in range(15):
-        carrot1.grow_plant()
-        screen.update()
-        print(f"Plant has grown for {i}")
-        time.sleep(2)
 
-    carrot1.reap_plant()
-    #hole_list_upper_left[4].clean_hole()
 
-    # for i in range(0,15):
-    #     for hole in hole_list_upper_left:
-    #         hole.water()
-    #         time.sleep(0.05)
-    #
-    # for i in range(0,15):
-    #     for hole in hole_list_upper_left:
-    #         hole.fertilize()
-    #         time.sleep(0.05)
-    #
-    # for i in range(0,15):
-    #     for hole in hole_list_upper_left:
-    #         hole.defertilize()
-    #         time.sleep(0.05)
 
-    screen.update()
+# #создаём лунки
+# hole_list_upper_left = []
+#
+# for hole in hole_list_upper_left_coordinates:
+#     hole_list_upper_left.append(Hole(hole[0],hole[1]))
 
-screen.mainloop()
+#indicator1 = WaterIndicator()
+#hole_list_upper_left[4].hole_indicator(indicator1)
+
+# hole_list_upper_left[4].watered_degree = 9
+# hole_list_upper_left[4].water()
+#
+# carrot1 = Carrot()
+# carrot2 = Carrot()
+# hole_list_upper_left[4].plant_plant(carrot1)
+# hole_list_upper_left[2].plant_plant(carrot2)
+#
+#
+#
+# game_continues = False
+# #screen.tracer(0)
+# #screen.update()
+# while game_continues:
+#     time.sleep(TIME_REFRESH)
+#
+#
+#     #for i in range(1): hole_list_upper_left[4].water(); time.sleep(0.5)
+#
+#     for i in range(15):
+#         carrot1.grow_plant()
+# #        indicator1.water_indicator_status(hole_list_upper_left[4].watered_degree)
+#         screen.update()
+#         print(f"Plant has grown for {i}")
+#         time.sleep(2)
+#
+#     carrot1.reap_plant()
+#     #hole_list_upper_left[4].clean_hole()
+#
+#     # for i in range(0,15):
+#     #     for hole in hole_list_upper_left:
+#     #         hole.water()
+#     #         time.sleep(0.05)
+#     #
+#     # for i in range(0,15):
+#     #     for hole in hole_list_upper_left:
+#     #         hole.fertilize()
+#     #         time.sleep(0.05)
+#     #
+#     # for i in range(0,15):
+#     #     for hole in hole_list_upper_left:
+#     #         hole.defertilize()
+#     #         time.sleep(0.05)
+#
+#     screen.update()
+
+#screen.mainloop()
